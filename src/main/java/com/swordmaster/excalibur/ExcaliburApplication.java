1bd624b1843121186a1905c6768a9615c191f617
ssoso27 <ssoyapdev@gmail.com>
2020-08-16 17:07:40 +0900

    etc: Spring boot 초기 프로젝트 생성
    - Spring boot initializer 를 통해 프로젝트를 생성함
    -- 적용 dependency : lombok, spring web
    - DB가 없는 상태에서 임시로 서버 run 시키기 위해 일부 dependency 주석처리
    -- 주석처리 dependency : Rest Repositories, Spring Data JPA, MySQL Driver
    
    issue : #27 

---
+++ src/main/java/com/swordmaster/excalibur/ExcaliburApplication.java
@@ -0,0 +1,13 @@
+package com.swordmaster.excalibur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcaliburApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcaliburApplication.class, args);
+	}
+
+}
