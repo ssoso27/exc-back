2b175781d81e56b79031415fd3d918576a59a31a
ssoso27 <ssoyapdev@gmail.com>
2020-10-04 23:08:43 +0900

    feat: 구글 로그인 기능 연동
    - /accounts/signin/google 로 호출 시 구글 로그인 연동
    - 프론트단에서 리다이렉션 관련 코드 작성 필요
    - 코드 정리 필요
    
     

---
+++ src/test/java/com/swordmaster/excalibur/controller/AccountControllerTest.java
@@ -0,0 +1,13 @@
+package com.swordmaster.excalibur.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccountControllerTest {
+
+    @Test
+    public void googleAuth {
+        
+    }
+}
