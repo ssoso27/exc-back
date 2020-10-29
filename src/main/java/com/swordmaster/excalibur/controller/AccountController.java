56a3783a48ad758961392fbab54e81fdd2ec1e87
ssoso27 <ssoyapdev@gmail.com>
2020-10-29 18:24:33 +0900

    docs: Account 관련 스웨거 문서 추가
    - AccountDTO, SignUpDTO, ResponseMessage 에 대한 설명 및 예시 추가
    - 회원가입, 로그인, 구글 로그인/회원가입 API에 대한 설명 추가
    
     

--- src/main/java/com/swordmaster/excalibur/controller/AccountController.java
+++ src/main/java/com/swordmaster/excalibur/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.swordmaster.excalibur.dto.SignUpAccountDTO;
 import com.swordmaster.excalibur.enumclass.Message;
 import com.swordmaster.excalibur.enumclass.UserRole;
 import com.swordmaster.excalibur.service.AccountService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    @ApiOperation(value = "(deprecated) 구글 로그인/회원가입", notes = "구글에서 리다이렉션을 받아 구글 로그인/회원가입을 진행합니다.")
     @GetMapping("/signin/google")
     public ResponseEntity<AccountDTO> googleSignin (@RequestParam(value="code") String authCode, @RequestParam(value="role") String role) throws JsonProcessingException {
         UserRole userRole = UserRole.valueOf(role.toUpperCase());
@@ -25,11 +27,13 @@ public class AccountController {
         return accountService.googleSignin(authCode, userRole);
     }
 
+    @ApiOperation(value="회원가입", notes = "정보를 받아 회원가입을 진행하고 결과에 따른 응답메세지를 반환합니다.")
     @PostMapping("/signup")
     public ResponseEntity<ResponseMessage> signUp(@RequestBody SignUpAccountDTO signUpAccountDTO) {
         return accountService.signUp(signUpAccountDTO);
     }
 
+    @ApiOperation(value="로그인", notes="로그인 성공 시 로그인 된 유저 정보와 jwt 토큰을 반환합니다.")
     @PostMapping("/signin")
     public ResponseEntity<AccountDTO> signIn(@RequestBody SignUpAccountDTO signUpAccountDTO) {
         return accountService.signIn(signUpAccountDTO);
