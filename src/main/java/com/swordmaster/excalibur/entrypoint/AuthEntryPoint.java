e07ea60cdb9b66112a6da25a6d0fa012e9a932bf
ssoso27 <ssoyapdev@gmail.com>
2020-11-07 16:06:33 +0900

    update: ResponseObject 추가
    - response 할 때 상황에 대한 response message 를 같이 주기 위해서 ResponseObject를 생성하고 감쌈
    
     

--- src/main/java/com/swordmaster/excalibur/entrypoint/AuthEntryPoint.java
+++ src/main/java/com/swordmaster/excalibur/entrypoint/AuthEntryPoint.java
@@ -1,6 +1,5 @@
 package com.swordmaster.excalibur.entrypoint;
 
-import com.swordmaster.excalibur.dto.ResponseMessage;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
