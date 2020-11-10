895c57782fd73ad1da1a0205e22d1440bb6147d5
ssoso27 <ssoyapdev@gmail.com>
2020-11-10 21:24:57 +0900

    update: uri 수정
    - 계층 간 관계에 맞춰서 uri를 수정함 (이런게 restful이라카던데)
    - path와 body의 분석세션 아이디가 일치해야 작동하도록 함
    
     

--- src/main/java/com/swordmaster/excalibur/controller/QuizController.java
+++ src/main/java/com/swordmaster/excalibur/controller/QuizController.java
@@ -2,18 +2,17 @@ package com.swordmaster.excalibur.controller;
 
 import com.swordmaster.excalibur.dto.QuizDTO;
 import com.swordmaster.excalibur.dto.ResponseObject;
+import com.swordmaster.excalibur.enumclass.Message;
 import com.swordmaster.excalibur.service.QuizService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/quizzes")
+@RequestMapping("/analysis-sessions/{analysisSessionId}/quizzes")
 public class QuizController {
     @Autowired
     QuizService quizService;
@@ -21,7 +20,11 @@ public class QuizController {
     @ApiOperation(value = "퀴즈 생성하기", notes = "강의자가 자신의 분석세션에 강의를 수동으로 추가합니다.")
     @PreAuthorize("isAuthenticated() and hasRole('ROLE_TEACHER')")
     @PostMapping("")
-    public ResponseEntity<ResponseObject> create(@RequestBody QuizDTO quizDTO) {
+    public ResponseEntity<ResponseObject> create(@PathVariable Integer analysisSessionId, @RequestBody QuizDTO quizDTO) {
+        if (!analysisSessionId.equals(quizDTO.getAnalysisSessionId())) {
+            return new ResponseEntity<>(new ResponseObject(Message.NOT_MATCH_PATH_BODY, null), HttpStatus.BAD_REQUEST);
+        }
+
         return quizService.create(quizDTO);
     }
 
